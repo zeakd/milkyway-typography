152965b50f1825dfdbf6f62d31611e7e2f624437
zeakd <artydeveloperduck@gmail.com>
2019-08-25 18:21:54 +0900

    fullscreen mode

---
+++ src/main.js
@@ -0,0 +1,39 @@
+const root = document.getElementById('root')
+
+
+
+function openFullscreen(elem) {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
+
+let fullscreened = false;
+
+root.addEventListener('click', () => {
+  if (fullscreened) {
+    closeFullscreen()
+    fullscreened = false;
+  } else {
+    openFullscreen(root);
+    fullscreened = true;
+  }
+})
